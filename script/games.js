13b97e08c168d70fd83109813dfbced4893944aa
DANIEL <cp.danniel@gmail.com>
2022-05-02 13:44:20 -0300

    xboxone added

--- script/games.js
+++ script/games.js
@@ -1,26 +1,26 @@
 let gameJson = [
-    {id:1, name:'PS3', img:'img/game1.png', price:64.99},
-    {id:2, name:'PS3', img:'img/game2.png', price:49.99},
-    {id:3, name:'PS3', img:'img/game3.png', price:69.99},
-    {id:4, name:'PS3', img:'img/game4.png', price:54.99},
+    {id:1, name:'Assassins Creed Revelations', img:'img/game1.png', price:44.99, link:'ps3/index.html'},
+    {id:2, name:'Assassins Creed II', img:'img/game2.png', price:49.99, link:'ps3/index.html'},
+    {id:3, name:'Battlefield 3', img:'img/game3.png', price:44.99, link:'ps3/index.html'},
+    {id:4, name:'Bioshok Infinite', img:'img/game4.png', price:39.99, link:'ps3/index.html'},
 
 
-    {id:5, name:'Alien Isolation', img:'img/game1.png', price:64.99},
-    {id:6, name:'Batman Arkham City', img:'img/game2.png', price:49.99},
-    {id:7, name:'Battlefield 4', img:'img/game3.png', price:69.99},
-    {id:8, name:'Borderlands 3', img:'img/game4.png', price:54.99},
+    {id:5, name:'Alien Isolation', img:'img/game5.png', price:64.99, link:'ps4/index.html'},
+    {id:6, name:'Batman Arkham City', img:'img/game6.png', price:49.99, link:'ps4/index.html'},
+    {id:7, name:'Battlefield 4', img:'img/game7.png', price:69.99, link:'ps4/index.html'},
+    {id:8, name:'Borderlands 3', img:'img/game8.png', price:54.99, link:'ps4/index.html'},
 
 
-    {id:9, name:'XBOX360', img:'img/game9.png', price:74.99},
-    {id:10, name:'XBOX360', img:'img/game10.png', price:64.99},
-    {id:11, name:'XBOX360', img:'img/game11.png', price:59.99},
-    {id:12, name:'XBOX360', img:'img/game12.png', price:74.99},
+    {id:9, name:'Battlefield 1', img:'img/game9.png', price:69.99, link:'xbox360/index.html'},
+    {id:10, name:'Devil May Cry 5', img:'img/game10.png', price:64.99, link:'xbox360/index.html'},
+    {id:11, name:'Darksiders', img:'img/game11.png', price:59.99, link:'xbox360/index.html'},
+    {id:12, name:'Quantum Break', img:'img/game12.png', price:54.99, link:'xbox360/index.html'},
 
 
-    {id:13, name:'XBOXone', img:'img/game13.png', price:74.99},
-    {id:14, name:'XBOXone', img:'img/game14.png', price:69.99},
-    {id:15, name:'XBOXone', img:'img/game15.png', price:49.99},
-    {id:16, name: 'XBOXone', img:'img/game16.png', price:79.99},
+    {id:13, name:'XBOXone', img:'img/game13.png', price:74.99, link:'xboxone/index.html'},
+    {id:14, name:'XBOXone', img:'img/game14.png', price:69.99, link:'xboxone/index.html'},
+    {id:15, name:'XBOXone', img:'img/game15.png', price:49.99, link:'xboxone/index.html'},
+    {id:16, name: 'XBOXone', img:'img/game16.png', price:79.99, link:'xboxone/index.html'},
 
 
     {id:17, name:'CONSOLES', img:'img/game17.png', price:69.99},
